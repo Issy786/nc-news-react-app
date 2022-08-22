85a4d8b8bc5636a55bb71b56879e57d2feb73638
Issy786 <issy_02@hotmail.com>
2022-08-22 16:32:50 +0100

    completed and updated ncnews-4

--- src/components/aticles-list.jsx
+++ src/components/aticles-list.jsx
@@ -3,12 +3,15 @@ export function ListArticle({ articles }) {
     <div>
       <ul>
         {articles.map((article) => {
+          console.log(article);
           return (
             <li className="article-list" key={article.article_id}>
               <h3>{article.title}</h3>
               <h4>Topic: {article.topic}</h4>
               <h4>Author: {article.author}</h4>
-              <h4>descripition: {article.body}</h4>
+              <h4>Date: {article.created_at}</h4>
+              <h4>Votes: {article.votes}</h4>
+              <h4>Comments: {article.comment_count}</h4>
             </li>
           );
         })}
