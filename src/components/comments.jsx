bd0aa7ad40dc954337a200497d49f3da239350aa
Issy786 <issy_02@hotmail.com>
2022-08-25 10:04:54 +0100

    completed ncnews-9 and errors handled

--- src/components/comments.jsx
+++ src/components/comments.jsx
@@ -1,33 +1,26 @@
 import { useEffect, useState } from "react";
 import { fetchAllCommentsByArticleId } from "../api";
+import { CommentList } from "./comments-list";
+import { PostComment } from "./post-comment";
 
 export const Comments = ({ article_id }) => {
   const [comments, setComments] = useState([]);
 
   useEffect(() => {
-    fetchAllCommentsByArticleId(article_id).then((data) => {
-      setComments(data.comments);
+    fetchAllCommentsByArticleId(article_id).then((res) => {
+      setComments(res.data.comments);
     });
   }, [article_id]);
 
   return (
     <div className="comments">
       <h3 className="comments-header">Comments</h3>
-
-      {comments.map((comment) => {
-        return (
-          <article className="comments-tiles" key={comment.comment_id}>
-            <p className="comment-date">
-              <span className="comment-author">{comment.author}</span>{" "}
-              {comment.created_at}
-            </p>
-            <p>{comment.body}</p>
-            <p className="comment-vote-count">
-              <span className="comment-votes">Votes</span> {comment.votes}
-            </p>
-          </article>
-        );
-      })}
+      <PostComment
+        comments={comments}
+        setComments={setComments}
+        article_id={article_id}
+      />
+      <CommentList comments={comments} />
     </div>
   );
 };
